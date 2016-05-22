e32ec9d86c0722025865331e0967bfb58af21307
Jiri Peinlich <jiri.peinlich@gmail.com>
2016-05-22 01:25:14 +0100

    refactored into api classes

--- thedaobot.rb
+++ thedaobot.rb
@@ -9,18 +9,39 @@ secret = YAML.load_file(ARGV[0])
 
 r=nil
 
-def connect secret
-	# Authorization
-	r = Redd.it(:script, secret['id'],secret['secret'],secret['username'],secret['password'], user_agent: "thedaobot v0.0.1")
-	r.authorize!
-end 
+class RedditApi
+    def initialize secret
+        @r = Redd.it(:script, secret['id'],secret['secret'],secret['username'],secret['password'], user_agent: "thedaobot v0.0.1")
+    end
 
-def checkDaoAccount secret, account
-	puts account
-	response = RestClient.get("https://api.etherscan.io/api?module=account&action=tokenbalance&tokenname=THEDAO&address=#{account}&apikey=#{secret['token']}")
-	puts response.code
-	puts response.to_str
+    def authorize!
+        @r.authorize!
+    end
+
+    def get_new_messages
+        @r.my_messages
+    end
+
+end
+
+class DAOApi
+    def initialize secret
+        @api_key = secret['token']
+    end
+
+    def account_details account
+        YAML.load RestClient.get("https://api.etherscan.io/api?module=account&action=tokenbalance&tokenname=THEDAO&address=#{account}&apikey=#{@api_key}")
+    end
+
+    def account_tokens account
+        account_details(account)["result"]
+    end
 end
 
 
-checkDaoAccount secret, "0x33d9b12b3b05927a1a00d5896017c5ff4967fca9"
+
+r = RedditApi.new secret
+d = DAOApi.new secret
+account = "0x33d9b12b3b05927a1a00d5896017c5ff4967fca9"
+puts d.account_details(account)['result']
+
