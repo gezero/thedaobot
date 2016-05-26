f56292a4bfa7033433bd239f1e3d911530faece3
Jiri Peinlich <jiri.peinlich@ingg.com>
2016-05-26 17:49:38 +0100

    finding of public address works

--- thedaobot.rb
+++ thedaobot.rb
@@ -58,29 +58,29 @@ def step secret
 end
 
 
-def extract_account_from_signature signature
+def extract_account_from_signature signature, message_hash
     r = signature.slice(0, 66)
     s = '0x' + signature.slice(66, 64)
     v = '0x' + signature.slice(130, 2)
     v = v.to_i(16)
     v += 27 if v<27
-    puts "r=#{r} s=#{s} v=#{v}"
-
-    message_hash="d030d9a04df643f62a1502b017f51c41a659268091abbd20e2de97b935724d7c"
 
     group = ECDSA::Group::Secp256k1
-
     sign = ECDSA::Signature.new r.to_i(16),s.to_i(16)
-    ECDSA::recover_public_key(group,message_hash, sign) do |k|
+
+    keys =[]
+    ECDSA::recover_public_key(group,[message_hash].pack('H*'), sign) do |k|
         if k!= nil
             pub = k.x.to_s(16)+k.y.to_s(16)
-            puts pub
-            puts Digest::SHA3.new(256).hexdigest(pub)
+            pub_b =[pub].pack('H*')
+            keys << (Digest::SHA3.new(256).hexdigest(pub_b))[-40..-1]
         end
     end
 
-
+    return keys
 end
 
+message_hash="d030d9a04df643f62a1502b017f51c41a659268091abbd20e2de97b935724d7c"
+keys = extract_account_from_signature "0xf4dffb108315560563e30657c1a5d7942f54bf321593797f08f84ff0601643e2683eb468ddd5f5d9c5bea00a9661beb6e042d335706763957f40f81d790e7aa301", message_hash
 
-extract_account_from_signature "0xf4dffb108315560563e30657c1a5d7942f54bf321593797f08f84ff0601643e2683eb468ddd5f5d9c5bea00a9661beb6e042d335706763957f40f81d790e7aa301"
+puts keys
