396fb167965d84e12b9c41a9e9b9d3e79fa207be
Jiri Peinlich <jiri.peinlich@ingg.com>
2016-05-27 08:26:56 +0100

    refactored method into a class

--- thedaobot.rb
+++ thedaobot.rb
@@ -43,77 +43,88 @@ class DAOApi
 end
 
 
-def extract_keys_from_signature signature, message_hash
-    r = signature.slice(0, 66)
-    s = '0x' + signature.slice(66, 64)
-    v = '0x' + signature.slice(130, 2)
-    v = v.to_i(16)
-    v += 27 if v<27
-
-    group = ECDSA::Group::Secp256k1
-    sign = ECDSA::Signature.new r.to_i(16),s.to_i(16)
-
-    #puts "s: #{signature} m_h:#{message_hash}"
-    #puts "r: #{r} s:#{s} v:#{v}"
-
-    keys =[]
-    ECDSA::recover_public_key(group,[message_hash].pack('H*'), sign) do |k|
-        if k!= nil
-            puts k
-            pub = k.x.to_s(16)+k.y.to_s(16)
-            pub_b =[pub].pack('H*')
-            keys << (Digest::SHA3.new(256).hexdigest(pub_b))[-40..-1]
-        end
+class DaoBot
+    def initialize secret
+        @r = RedditApi.new secret
+        @d = DAOApi.new secret
     end
 
-    return keys
+    def extract_keys_from_signature signature, message_hash
+        r = signature.slice(0, 66)
+        s = '0x' + signature.slice(66, 64)
+        v = '0x' + signature.slice(130, 2)
+        v = v.to_i(16)
+        v += 27 if v<27
+
+        group = ECDSA::Group::Secp256k1
+        sign = ECDSA::Signature.new r.to_i(16),s.to_i(16)
+
+        #puts "s: #{signature} m_h:#{message_hash}"
+        #puts "r: #{r} s:#{s} v:#{v}"
+
+        keys =[]
+        ECDSA::recover_public_key(group,[message_hash].pack('H*'), sign) do |k|
+            if k!= nil
+                puts k
+                pub = k.x.to_s(16)+k.y.to_s(16)
+                pub_b =[pub].pack('H*')
+                keys << (Digest::SHA3.new(256).hexdigest(pub_b))[-40..-1]
+            end
+        end
+
+        return keys
+    end
+
+    def authorize!
+        @r.authorize!
+    end
+
+    def step 
+        puts "fetching new messages..."
+        @r.get_new_messages.each do |m|
+            sleep 2
+
+            next if m.read?
+
+            author = m.author
+            body = m.body
+
+            message_hash = Digest::SHA3.new(256).hexdigest(author)
+
+            match = /Signature: (0x\p{XDigit}*)/.match(body)
+
+            signature = match[1] if match
+            puts "User: #{author} message_hash = #{message_hash} signature=#{signature}"
+
+            if signature
+                keys = extract_keys_from_signature signature, message_hash
+
+                total = 0
+                keys.each do |k|
+                    amount = @d.account_details(k)['result'].to_i
+                    puts "key: #{k} amount:#{amount}"
+                    total += amount
+                end
+
+                puts "total = #{total}"
+
+                if total>0
+                    m.reply("The signature from #{author} is valid and related DAO account contains #{total}.\n\n I am a bot. [Report problem](https://www.reddit.com/r/thedaobot/) or [use me](https://www.reddit.com/r/thedaobot/wiki/thedaobot/usage).")
+                end
+
+            end
+            m.mark_as_read
+        end
+
+    end
+
 end
 
 
-def step secret
-    r = RedditApi.new secret
-    d = DAOApi.new secret
-
-    r.authorize!
-
-    messages = r.get_new_messages
-
-    messages.each do |m|
-        sleep 2
-
-        next if m.read?
-
-        author = m.author
-        body = m.body
-
-        message_hash = Digest::SHA3.new(256).hexdigest(author)
-
-        match = /Signature: (0x\p{XDigit}*)/.match(body)
-
-        signature = match[1] if match
-        puts "User: #{author} message_hash = #{message_hash} signature=#{signature}"
-
-        if signature
-            keys = extract_keys_from_signature signature, message_hash
-
-            total = 0
-            keys.each do |k|
-                amount = d.account_details(k)['result'].to_i
-                puts "key: #{k} amount:#{amount}"
-                total += amount
-            end
-
-            puts "total = #{total}"
-
-            if total>0
-                m.reply("The signature from #{author} is valid and related DAO account contains #{total}.\n\n I am a bot. [Report problem](https://www.reddit.com/r/thedaobot/) or [use me](https://www.reddit.com/r/thedaobot/wiki/thedaobot/usage).")
-            end
-
-        end
-        m.mark_as_read
-    end
-
-end
 
 
-step secret
+bot = DaoBot.new secret
+
+bot.authorize!
+
+bot.step
